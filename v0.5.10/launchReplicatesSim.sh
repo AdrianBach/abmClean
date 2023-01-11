af71029c706702364700379bc1dac9444d52c2b0
Adrian Bach <adrian.bach@stir.ac.uk>
2023-01-11 13:08:26 +0000

    test scenario

--- v0.5.10/launchReplicatesSim.sh
+++ v0.5.10/launchReplicatesSim.sh
@@ -19,7 +19,7 @@
 ## User defined variables ##
 
 # landscape variables
-size=5       # argv[2] world's side size
+size=25       # argv[2] world's side size
 res_nb=2     # argv[3] number of resource types
 max_res_1=100 # argv[4] max resource 1 per cell
 max_res_2=200 # argv[5] max resource 2 per cell
@@ -37,11 +37,11 @@ pry_cons_2=10	# argv[12] prey 2 max consumption in resource units
 # pry_surv_1=3  # argv[13] prey 1 resource units needed to pass survival trial
 # pry_surv_2=3  # argv[14] prey 2 resource units needed to pass survival trial
 pry_offs_1=1    # argv[15] prey 1 max number of offspring
-pry_offs_2=1    # argv[16] prey 2 max number of offspring
+pry_offs_2=3   # argv[16] prey 2 max number of offspring
 # pry_repr_1=5  # argv[17] prey 1 resource units needed to pass reproduction trial
 # pry_repr_2=5  # argv[18] prey 2 resource units needed to pass reproduction trial
 pry_intro_1=0   # argv[19]
-pry_intro_2=11   # argv[20]
+pry_intro_2=401   # argv[20]
 
 # predator variables
 prd_nb=1        # argv[21] number of predator types
@@ -49,26 +49,26 @@ prd_init_1=100    # argv[22] predator 1 initial density in nb of individuals
 prd_move_1=0.1  # argv[23] predator 1 max movement range in fraction of size
 prd_surv_1=300	# arg[25]
 prd_offs_1=0.5    # argv[26] predator 1 max number of offspring
-prd_intr_1=6    # argv[28] predator 1 time of introduction in the model
+prd_intr_1=201    # argv[28] predator 1 time of introduction in the model
 prd_ctch_pry1_1=0.1  # argv[29] predator 1 prey1 catch probability
 prd_ctch_pry2_1=0.1  # argv[30] predator 1 prey2 catch probability
-prd_oprt_1=0    # argv[33] is predator oportunistic? (0 or 1)
 prd_cvrt_pry1_1=100  # argv[31] predator 1 prey1 resources/catch
 prd_cvrt_pry2_1=100  # argv[32] predator 1 prey1 resources/catch
+prd_oprt_1=0    # argv[33] is predator oportunistic? (0 or 1)
 prd_spcf_1=0    # argv[34] is predator specific? (0 or 1)
 
 # time variables
-simu_time=20    # argv[35] simulation time
-freq_repr=5   # argv[36] frequency of reproduction trials
-freq_surv=5    # argv[37] frequency of survival trials
-freq_rfll=5    # argv[38] frequency of landscape resources refill
+simu_time=1000    # argv[35] simulation time
+freq_repr=10   # argv[36] frequency of reproduction trials
+freq_surv=10    # argv[37] frequency of survival trials
+freq_rfll=10    # argv[38] frequency of landscape resources refill
 
 # frequency of assessment
-freq_rslt=1    # argv[39] frequency of landscape results shot
-freq_snap=1  # argv[40] frequency of snap measure
+freq_rslt=10    # argv[39] frequency of landscape results shot
+freq_snap=1001  # argv[40] frequency of snap measure
 
 # number of replicates
-rep=1
+rep=10
 
 ## Non user defined variables ##
 
@@ -91,7 +91,7 @@ prd_repr_1=$(($prd_surv_1/1)); # echo "prd_repr_1 = $prd_repr_1" # argv[27] pred
 # prd_cvrt_pry2_1=$(($ratio*$prd_cvrt_pry1_1))  # argv[32] predator 1 prey1 resources/catch
 
 # name the simulation with only the variables of interest and their value
-sim_name="test-res2" # argv[1]
+sim_name="test-introScenario" # argv[1]
 
 ## Warning about timing of introduction ##
 # if [[ $varA == 1 && ($varB == "t1" || $varC == "t2") ]]; then
@@ -174,7 +174,7 @@ printf "pry_offs_2 = $pry_offs_2 \t # argv[$p] prey 2 max number of offspring\n"
 p=$(($p+1))
 printf "pry_repr_1 = $pry_repr_1 \t # argv[$p] prey 1 resource units needed to pass reproduction trial\n" >> paramFile.txt
 p=$(($p+1))
-printf "pry_repr_2 = $pry_repr_2 \t # argv[$p] prey 2 resource units needed to pass reproduction trial\n\n" >> paramFile.txt
+printf "pry_repr_2 = $pry_repr_2 \t # argv[$p] prey 2 resource units needed to pass reproduction trial\n" >> paramFile.txt
 p=$(($p+1))
 printf "pry_intro_1 = $pry_intro_1 \t # argv[$p] prey 1 time step of introduction \n" >> paramFile.txt
 p=$(($p+1))
