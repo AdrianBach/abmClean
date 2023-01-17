c482bf795198e12b23b2490f5f08ff6d2ca0cbbd
Adrian Bach <adrian.bach@stir.ac.uk>
2023-01-17 17:14:57 +0000

    higher prey2 init in newSA-py2offs0.1

--- v0.5.9/launchReplicatesSim.sh
+++ v0.5.9/launchReplicatesSim.sh
@@ -29,7 +29,7 @@ max_res_2=100 # argv[5] max resource 2 per cell
 # prey variables
 pry_nb=2        # argv[6] number of prey types
 pry_init_1=100   # argv[7] prey 1 initial density in nb of individuals
-pry_init_2=100   # argv[8] prey 2 initial density
+pry_init_2=1200   # argv[8] prey 2 initial density
 pry_move_1=0.1  # argv[9] prey 1 max movement range in fraction of size
 pry_move_2=0.1  # argv[10] prey 2 movement range
 pry_cons_1=10	# argv[11] prey 1 max consumption in resource units
@@ -37,7 +37,7 @@ pry_cons_2=10	# argv[12] prey 2 max consumption in resource units
 # pry_surv_1=3  # argv[13] prey 1 resource units needed to pass survival trial
 # pry_surv_2=3  # argv[14] prey 2 resource units needed to pass survival trial
 pry_offs_1=1    # argv[15] prey 1 max number of offspring
-pry_offs_2=1    # argv[16] prey 2 max number of offspring
+pry_offs_2=0.1    # argv[16] prey 2 max number of offspring
 # pry_repr_1=5  # argv[17] prey 1 resource units needed to pass reproduction trial
 # pry_repr_2=5  # argv[18] prey 2 resource units needed to pass reproduction trial
 pry_intro_1=0   # argv[19]
@@ -50,8 +50,8 @@ prd_move_1=0.1  # argv[23] predator 1 max movement range in fraction of size
 prd_surv_1=300	# arg[25]
 prd_offs_1=1    # argv[26] predator 1 max number of offspring
 prd_intr_1=201    # argv[28] predator 1 time of introduction in the model
-prd_ctch_pry1_1=0.4  # argv[29] predator 1 prey1 catch probability
-prd_ctch_pry2_1=0.4  # argv[30] predator 1 prey2 catch probability
+prd_ctch_pry1_1=0.1  # argv[29] predator 1 prey1 catch probability
+prd_ctch_pry2_1=0.1  # argv[30] predator 1 prey2 catch probability
 prd_cvrt_pry1_1=100  # argv[31] predator 1 prey1 resources/catch
 prd_cvrt_pry2_1=100  # argv[32] predator 1 prey1 resources/catch
 prd_oprt_1=0    # argv[33] is predator oportunistic? (0 or 1)
@@ -68,7 +68,7 @@ freq_rslt=10    # argv[39] frequency of landscape results shot
 freq_snap=1001  # argv[40] frequency of snap measure
 
 # number of replicates
-rep=10
+rep=30
 
 ## Non user defined variables ##
 
@@ -91,7 +91,7 @@ prd_repr_1=$(($prd_surv_1/1)); # echo "prd_repr_1 = $prd_repr_1" # argv[27] pred
 # prd_cvrt_pry2_1=$(($ratio*$prd_cvrt_pry1_1))  # argv[32] predator 1 prey1 resources/catch
 
 # name the simulation with only the variables of interest and their value
-sim_name="nullCalibNrw-py1Init100-prdSurv300-prdCtPr0.4" # argv[1]
+sim_name="newnewSA-py2offs0.1" # argv[1]
 
 ## Warning about timing of introduction ##
 #if [[ ( $P = "SFTP" && $PORT != "22" ) || ( $P = "FTPS" && $PORT != "990" && $PORT != "21" ) ]] ; then
